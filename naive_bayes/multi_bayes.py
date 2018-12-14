c9539cc3dbfe4b666f30496d9c6a5ce4121c7bb2
Annie Lydens <annie@thoughtbot.com>
2018-12-14 15:05:48 -0600

    Clean up GaussianNB model

--- naive_bayes/multi_bayes.py
+++ naive_bayes/multi_bayes.py
@@ -1,19 +1,38 @@
 import pandas as pd
-import pdb
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
+from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 
 # Read in Data
-df = pd.read_csv('FINAL_demo_reac_outc.csv', sep=',')
+df = pd.read_csv('SAMPLE_demo_reac_outc.csv', sep=',')
 
 # Clean and Preprocess
-df['life_threatening_outcome'] = df.outc_cod.map({'LT': 1, 'DE': 1, 'OT': 0, 'HO': 0, 'RI': 0, 'DS': 0 , 'CA': 0 })
+df['life_threatening_outcome'] = df.outc_cod.map({
+    'LT': 1,
+    'DE': 1,
+    'OT': 0,
+    'HO': 0,
+    'RI': 0,
+    'DS': 0 ,
+    'CA': 0
+})
 
 df["country_cleaned"] = df.occr_country.map({
-    'AU': 11, 'CA': 6, 'CH': 8, 'CN': 5, 'CO': 9, 'DE': 4, 'ES': 13, 'GB': 3, "IT": 10, "JP": 7, "RO": 12, "TR": 2, "US": 1
+    'AU': 11,
+    'CA': 6,
+    'CH': 8,
+    'CN': 5,
+    'CO': 9,
+    'DE': 4,
+    'ES': 13,
+    'GB': 3,
+    'IT': 10,
+    'JP': 7,
+    'RO': 12,
+    'TR': 2,
+    'US': 1
 })
 
 def clean_weights(row):
@@ -33,14 +52,22 @@ df = df[pd.notnull(df['wt'])]
 df = df[pd.notnull(df['weight_cleaned'])]
 df = df[pd.notnull(df['occr_country'])]
 df = df[pd.notnull(df['age'])]
-df = df[df['age'] < 120]
-df = df[df['age'] > 0]
-df = df[df['weight_cleaned'] < 500]
-df = df[df['weight_cleaned'] > 0]
-df['country_cleaned'] = df['country_cleaned'].fillna(14)
 
-df['binned_age'] = df['age'].values.round(-1)
-df['binned_weight'] = df['wt'].values.round(-1)
+# Remove outliers from age
+age_q1 = df['age'].quantile(0.25)
+age_q3 = df['age'].quantile(0.75)
+boundary = 1.5 * (age_q3 - age_q1)
+df = df[df['age'] < (age_q3 + boundary)]
+df = df[df['age'] > (age_q1 - boundary)]
+
+# Remove outliers from weight
+weight_q1 = df['weight_cleaned'].quantile(0.25)
+weight_q3 = df['weight_cleaned'].quantile(0.75)
+boundary = 1.5 * (weight_q3 - weight_q1)
+df = df[df['weight_cleaned'] < (weight_q3 + boundary)]
+df = df[df['weight_cleaned'] > (weight_q1 - boundary)]
+
+df['country_cleaned'] = df['country_cleaned'].fillna(14)
 
 df['demo_gender'] = np.where(df["sex"] == "M",1,0)
 
@@ -49,9 +76,7 @@ df = df[[
     "age",
     "wt",
     "wt_cod",
-    "binned_weight",
     "weight_cleaned",
-    "binned_age",
     "country_cleaned",
     "life_threatening_outcome"
 ]]
@@ -67,31 +92,23 @@ used_features =[
     "country_cleaned"
 ]
 
-X = df[used_features].values
-Y = np.array(df["life_threatening_outcome"].values)
+features = df[used_features].values
+labels = np.array(df["life_threatening_outcome"].values)
 
-# Split Data into training sets
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.4, random_state=1)
+# Split into training and test sets
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.4, random_state=1)
 
-# Initialize and train model
+# Train Model
 mnb = GaussianNB()
-# mnb = MultinomialNB()
-# mnb = BernoulliNB()
-mnb.fit(X_train, Y_train)
-
-Y_pred = mnb.predict(X_test)
+mnb.fit(features_train, labels_train)
+labels_pred = mnb.predict(features_test)
 
 # Print Report
-print(df.groupby('life_threatening_outcome').size())
-print(df.groupby('demo_gender').size())
+print("GaussianNB model accuracy:", metrics.accuracy_score(labels_test, labels_pred)*100)
 
-print("Multinomial Naive Bayes model accuracy(in %):", metrics.accuracy_score(Y_test, Y_pred)*100)
-
-print("Given female, 30, 80kg probability of a serious outcome:")
-weight = 80
-age = 60
-male = 1
-country_int_code = 1
-input = [male, age, weight, country_int_code]
-prob1 = mnb.predict_proba([input])
-print("Prob of US serious outcome (in %):", prob1[0][1]*100)
+# Example Patient Profile
+print("Given US male, 60 yrs old, 80kg:")
+weight, age, is_male, country_int_code = 80, 60, 1, 1
+input = [is_male, age, weight, country_int_code]
+patient_prob = mnb.predict_proba([input])
+print("% probability of serious outcome:", patient_prob[0][1]*100)
